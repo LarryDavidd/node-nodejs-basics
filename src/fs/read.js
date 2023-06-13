38855de02ac444d403d105022d591b555fd3b337
Airat2000 <gerasimovevgeni16082003@gmail.com>
2023-06-13 19:18:14 +0400

    feat: finish fs folder

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,12 @@
-const read = async () => {
-    // Write your code here 
+const fs = require("fs");
+const path = require("path");
+
+const read = () => {
+  if (!fs.existsSync(path.join(__dirname, 'files', 'fileToRead.txt'))) {
+    throw new Error('FS operation failed');
+  } else {
+    fs.createReadStream(path.join(__dirname, 'files', 'fileToRead.txt'), "utf-8").on("data", (chunk) => console.log(chunk));
+  }
 };
 
-await read();
+read();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
-const rename = async () => {
-    // Write your code here 
+const fs = require("fs");
+const path = require("path");
+
+const rename = () => {
+  if (!fs.existsSync(path.join(__dirname, 'files', 'wrongFilename.txt')) || 
+       fs.existsSync(path.join(__dirname, 'files', 'properFilename.md')))
+  {
+    throw new Error('FS operation failed');
+  } else {
+    fs.rename(
+      path.join(__dirname, 'files', 'wrongFilename.txt'), 
+      path.join(__dirname, 'files', 'properFilename.md'), 
+      (err) => {
+        if (err) return err;
+        console.log(`file wrongFilename.txt was renamed`);
+      }
+    );
+  }
 };
 
-await rename();
+rename();
