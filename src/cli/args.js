11ee7cca05b9356d684e469dd3699685f4271598
Airat2000 <gerasimovevgeni16082003@gmail.com>
2023-06-17 21:55:48 +0400

    feat: finish cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
+const { argv, stdout } = process;
+
 const parseArgs = () => {
-    // Write your code here 
+  const arg = argv.slice(2);
+  for (let i = 0; i < arg.length; i += 2) {
+    stdout.write(`${arg[i].slice(2)} is ${arg[i + 1]}, `);
+  }
 };
 
 parseArgs();
