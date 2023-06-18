a94170a607d6080eb002fe067cfe20797ae73e5c
Airat2000 <gerasimovevgeni16082003@gmail.com>
2023-06-18 21:59:17 +0400

    feat: finish module tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,11 @@
-const read = async () => {
-    // Write your code here 
+import fs from "fs";
+import path from "path";
+import { stdout } from "process";
+
+const read = () => {
+    const stream = fs.createReadStream((path.join("src", "streams", "files", "fileToRead.txt")), "utf-8");
+    stream.on("data", (chunk) => stdout.write(chunk));
+    stream.on("error", e => console.log(e.message));
 };
 
-await read();
+read();
