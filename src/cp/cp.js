7a2156b1f77eacbd53bf4a5b342e6ab72e2766bb
Airat2000 <gerasimovevgeni16082003@gmail.com>
2023-06-19 21:30:22 +0400

    feat: create spawn function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { spawn } from 'child_process';
+import path from 'path';
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+    spawn('node', ['./' + path.join('src', 'cp', 'files', 'script.js'), ...args], {
+        stdio: [process.stdin, process.stdout, 'ipc'],
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3]);
